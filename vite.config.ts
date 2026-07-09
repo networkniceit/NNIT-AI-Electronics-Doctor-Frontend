532ea0be3c32d5da14fa666f7bcb3f770c41c975
networkniceit <networkniceit@gmail.com>
2026-07-09 15:38:09 +0200

    Fix emoji UTF-8 encoding in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+﻿import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    charset: "utf8",
+  },
+  server: {
+    headers: {
+      "Content-Type": "text/html; charset=utf-8",
+    },
+  },
 })
