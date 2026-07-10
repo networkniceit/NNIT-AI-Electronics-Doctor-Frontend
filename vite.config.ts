f522cb9f8d6508dd6e00b9f6c821013aa6dad4aa
networkniceit <networkniceit@gmail.com>
2026-07-10 06:36:52 +0200

    Fix vite config remove invalid charset option

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,4 @@ import react from "@vitejs/plugin-react"
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    charset: "utf8",
-  },
-  server: {
-    headers: {
-      "Content-Type": "text/html; charset=utf-8",
-    },
-  },
 })
